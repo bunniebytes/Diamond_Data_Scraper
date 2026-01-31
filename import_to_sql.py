9b34d031892ca4b28f53bd2ac958d6c66aa8ed1a
Bunnie <bunniecodes@gmail.com>
2026-01-31 00:42:27 -0800

    Most of data is cleaned and can able to be written to csv and sql db.  Need to still address regions as well as events.

---
+++ import_to_sql.py
@@ -0,0 +1,57 @@
+import sqlite3
+import pandas as pd
+import datetime
+from contextlib import closing
+
+class ConvertToSQL():
+    def __init__(self):
+        pass
+    
+    def run(self):
+        self.convert_csv_to_sql("player_hit.csv")
+        self.convert_csv_to_sql("player_pitch.csv")
+        self.convert_csv_to_sql("standing.csv")
+        self.convert_csv_to_sql("team_hit.csv")
+        self.convert_csv_to_sql("team_pitch.csv")
+    
+    def convert_csv_to_sql(self, file):
+        table_name = file.replace(".csv", "")
+        df = pd.read_csv(file)
+        self.clean_data(df)
+        
+        with closing(sqlite3.connect("baseball_data.db")) as conn:
+            print("Database successfully opened.")
+            df.to_sql(table_name, conn, if_exists = "replace", index = True, index_label = "row_id")
+
+    def clean_data(self, df):
+        self.combine_duplicate_columns(df)
+        self.standardize_column_types(df)
+        print(df.head())
+        
+    def combine_duplicate_columns(self, df):
+        column_names = df.columns.str.strip()
+        print(column_names)
+        duplicate_mapping = {"Wins" : "W",
+                "Losses" : "L",
+                "Ties" : "T"}
+        for full_stat, letter_stat in duplicate_mapping.items():
+            if full_stat in column_names and letter_stat in column_names:
+                df[full_stat] = pd.to_numeric(df[full_stat], errors = "coerce")
+                df[letter_stat] = pd.to_numeric(df[letter_stat], errors = "coerce")
+                df[full_stat] = df[letter_stat].fillna(df[full_stat]).fillna(0).astype(int)
+                df.drop(columns = letter_stat, errors = "ignore", inplace = True)
+    
+    def standardize_column_types(self, df):
+        column_names = df.columns
+        type_mapping = {"Year" : int,
+                        "#" : float,
+                        # "Wins" : int,
+                        # "Losses" : int,
+                        # "Ties" : int,
+                        "GB" : float}
+        for column_name, data_type in type_mapping.items():
+            if column_name in column_names:
+                df[column_name] = pd.to_numeric(df[column_name], errors = "coerce").fillna(0).astype(data_type)
+                
+if __name__ == "__main__":
+    ConvertToSQL().run()
--- scraper_logic.py
+++ scraper_logic.py
@@ -3,12 +3,15 @@ import pandas as pd
 import json
 from time import sleep
 import re
+from io import StringIO
 from collections import defaultdict
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Define a directory for the user profile (cache and cookies will be saved here)
 profile_dir = os.path.abspath('selenium_profile')
@@ -26,6 +29,7 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 
 class Scraper():
     def __init__(self):
+        self.scraped_pages = {}
         self.events = {}
         self.player_stats = defaultdict(dict)
         self.team_stats = defaultdict(dict)
@@ -34,20 +38,28 @@ class Scraper():
         try:
             links = self.get_year_links("https://www.baseball-almanac.com/yearmenu.shtml")
             # links = ["https://www.baseball-almanac.com/yearly/yr1887n.shtml", "https://www.baseball-almanac.com/yearly/yr1970n.shtml"]
-            self.log_data(links)
-            # self.log_data(["https://www.baseball-almanac.com/yearly/yr1970n.shtml", "https://www.baseball-almanac.com/yearly/yr1986n.shtml", "https://www.baseball-almanac.com/yearly/yr1887n.shtml", "https://www.baseball-almanac.com/yearly/yr1883n.shtml", "https://www.baseball-almanac.com/yearly/yr1934a.shtml"])
+            self.log_html(links)
+            # self.log_html(["https://www.baseball-almanac.com/yearly/yr1970n.shtml", "https://www.baseball-almanac.com/yearly/yr1986n.shtml", "https://www.baseball-almanac.com/yearly/yr1887n.shtml", "https://www.baseball-almanac.com/yearly/yr1883n.shtml", "https://www.baseball-almanac.com/yearly/yr1934a.shtml"])
+            # self.log_data()
+            
+            # Testing purposes saved all the html into a csv so I can test my code without constantly scraping website for the html.
+            # df = pd.DataFrame(self.scraped_pages.items(), columns=['URL', 'HTML_Content'])
+            # df.to_csv("test.csv", index = False)
             
         except Exception as e:
             print("Unable to open the url provided.")
             print(f"Exception: {type(e).__name__} {e}")
 
+        # data = pd.read_csv("test.csv")
+        # html_strings = data["HTML_Content"]
+        # self.log_data(html_strings)
         player_hit_df, player_pitch_df, player_standing_df = self.convert_stats_to_df(self.player_stats)
         team_hit_df, team_pitch_df, standing_df = self.convert_stats_to_df(self.team_stats)
+        # events_df = pd.DataFrame(self.events)
         
         # # TODO THIS IS TEST TO MAKE SURE DATA IS CORRECT
         # temp = pd.json_normalize(self.player_stats)
         # temp.to_csv("test.csv", index = False)
-        
         print(player_hit_df)
         print(player_pitch_df)
         player_hit_df.to_csv("player_hit.csv", index = False)
@@ -55,40 +67,46 @@ class Scraper():
         team_hit_df.to_csv("team_hit.csv", index = False)
         team_pitch_df.to_csv("team_pitch.csv", index = False)
         standing_df.to_csv("standing.csv", index = False)
+        # events_df.to_csv("events.csv", index=False)
         
+
         driver.quit()
   
     def get_year_links(self, link):
         driver.get(link)
         search_results = driver.find_elements(By.CSS_SELECTOR, "table.ba-sub > tbody > tr > td.datacolBox > a")
         # only scraping data for the American and National leagues
-        pattern = r"yr\d{4}(a|n)\.shtml$"
+        pattern = r"yr(?!(?:188[2-9]|189[01])a)\d{4}(a|n).shtml"
         links = [link.get_attribute("href") for link in search_results if re.search(pattern, link.get_attribute("href"))]
     
         return links
     
-    # This gets the driver for the new page
-    def get_driver_new_page(self, link):
-        driver.get(link)
-    
-    def get_year_league(self, driver):
+    # This gets the html for the new page and stores it in a dictionary with the link.  Also checks to make sure it is only for American and National League
+    def get_html(self, driver, link):
+        html_string = driver.find_element(By.XPATH, "//div[starts-with(@class, 'container') or starts-with(@id, 'container')][.//table]").get_attribute("innerHTML")
+        self.scraped_pages[link] = html_string
+        
+    def get_year_league(self, html_string : str):
         # pulling the header from the intro to get the year and the league
-        scraped_data = driver.find_element(By.CSS_SELECTOR, "div.intro > h1")
-        pattern = r"\d{4}\s(AMERICAN|NATIONAL)\sLEAGUE"
-        try:
-            search_result = re.search(pattern, scraped_data.text).group()
-            if search_result:
-                year, league = search_result.split(" ", 1)
-                year, league = int(year), league.title()
+        pattern = r"(?i)\d{4}\s(american|national)\sleague"
+        search_result = re.search(pattern, html_string).group()
+        if search_result:
+            year, league = search_result.split(" ", 1)
+            year, league = int(year), league.title()
             if (year >= 1901 and league == "American League") or league == "National League":
+                # print(f"Successfully retrieved {year} {league}")
                 return year, league
         # TODO This is being raised because American Association has link that also ends in a.  Need to fix
-        except Exception:
-            pass
 
+    # find's all the tbody tags in the html string
+    def find_tables(self, html_string : str):
+        all_tables = pd.read_html(StringIO(html_string))
+        # print(f"Found {len(all_tables)} tables on the page.")
         
-    # TODO Make this smaller functions T_T
-    def get_data(self, driver):
+        return all_tables
+    
+    # TODO Make this smaller functions T_T Need to delete?
+    # def get_data(self, driver):
         player_stats_dict = {}
         team_stats_dict = {}
         search_results = driver.find_elements(By.CSS_SELECTOR, "table.boxed")
@@ -105,7 +123,7 @@ class Scraper():
                 # league_pattern = r"(American|National)\sLeague"
                 temp_table_name, temp_col_num = self.find_table_name_and_columns(row)
                 temp_col_names, temp_dup_rows = self.find_col_names(row)
-                data, temp_dup_rows = self.find_cell_data(row, col_num, duplicate_rows)
+                data, temp_dup_rows = self.find_table_data(row, col_num, duplicate_rows)
                 if temp_table_name:
                     table_name = temp_table_name
                 if temp_col_num:
@@ -132,118 +150,115 @@ class Scraper():
                     
         return player_stats_dict, team_stats_dict
     
-    def find_table_name_and_columns(self, row):
+    def find_table_name(self, table : list):
+        # Know that the table name will always be 0 key in the list
         table_name = []
         player_pattern = r"(Player|Pitcher)"
-        team_pattern = r"Team(?= Review)|Team Standings"
-        stat_name = r"^.+Statistics"
-        try:
-            headers = [header.text for header in row.find_elements(By.XPATH, ".//h2 | .//p")]
-        except:
-            pass
-        if not headers:
-            return None, None
-        num_cols = row.find_element(By.TAG_NAME, "td").get_attribute("colspan")
+        team_pattern = r"Team(?=\sReview)|Team Standings"
+        stat_name = r"(Hitting|Pitching)\sStatistics"
+        player = "Player"
+        col = table[0][0]
+        # TODO get rid of this bandaid
+        bandaid = table[len(table) - 1][0]
 
-        if match := re.search(player_pattern, headers[0]):
-            player = "Player"
+        if match := re.search(player_pattern, col):
             table_name.append(player)
-        if match := re.search(team_pattern, headers[0]) or (match := re.search(team_pattern, headers[1])):
+        if (match := re.search(team_pattern, col)) or (match := re.search(team_pattern, bandaid)):
             team = match.group().split(" ")
             table_name.extend(team)
-        if match := re.search(stat_name, headers[1]):
-            stat = match.group()
+        if match := re.search(stat_name, col):
+            stat = match.group(0)
             table_name.append(stat)
-            
-        return table_name, int(num_cols)
+        # if len(table_name) == 1:
+        #     print(team)
+        return table_name
     
-    def find_col_names(self, row):
-        try:
-            elements = row.find_elements(By.XPATH, ".//td[contains(@class, 'banner')]")
-        except:
-            pass
-        col_names = []
-        duplicate_row_val = {}
-        if not elements:
-            return None, None
-        regions = ["East", "Central", "West"]
-        for idx, name in enumerate(elements):
-            num_rows = name.get_attribute("rowspan")
-            if num_rows:
-                duplicate_row_val[idx] = [name.text, int(num_rows)]
-            if name.text in regions:
-                col_names.append("Region")
-            else:
-                col_names.append(name.text.replace(" [Click for roster]", "").strip())
-        return col_names, duplicate_row_val
+    def find_col_names(self, table : list):
+        # the column names will be index 1 in the list of rows
+        col_names = [col for col in table[1].values() if col != "Top 25"]
+        # col_names = [col.replace(" [Click for roster]", "").replace(" | Roster", "").replace("(s)", "") for col in table[1].values() if isinstance(col, str)]
+        return col_names
 
-    def find_cell_data(self, row, num_cols, duplicate_rows):
-        try:
-            cells = row.find_elements(By.XPATH, ".//td[contains(@class, 'datacolBox') or contains(@class, 'datacolBlue')]")
-        except:
-            pass
-        if not cells:
-            return None, duplicate_rows
-        data = []
-        for idx, cell in enumerate(cells):
-            num_rows = cell.get_attribute("rowspan")
-            if num_rows:
-                duplicate_rows[idx] = [cell.text, int(num_rows)]
-            data.append(cell.text.strip())
-        if len(data) != num_cols:
-            for idx, value in duplicate_rows.items():
-                data.insert(idx, value[0])
-                duplicate_rows[idx][1] -= 1
-        duplicate_rows = {k: v for k, v in duplicate_rows.items() if v[1] > 0}
-        # if len(cells) > 1 and len(cells) == len(col_names):
-        #     prev_cells = cells
-        #     cell_results.append(cells)
-        return data, duplicate_rows
+    def find_table_data(self, col_names : list, table):
+        # We know the data starts at the second index of the list.  We want to return a list of dictionaries, each dictionary for 1 row in the table with column name as the key
+        events = None
+        events_pattern = r".*(Seasonal|Salary).*"
+        table_data = []
+        
+        # TODO need to address the East/West/Central issue for the Standing table
+        for row in table[2::]:
+            row_dict = {}
+            for idx, col_name in enumerate(col_names):
+                row_values = [value for value in row.values()]
+                current_val = str(row_values[idx])
+                if match := re.search(events_pattern, current_val):
+                    events = match.group()
+                if col_name != row_values[idx] and len(set(row_values)) != 1:
+                    row_dict[col_name.replace(" [Click for roster]", "").replace(" | Roster", "").replace("(s)", "")] = row_values[idx]
+            if row_dict:
+                table_data.append(row_dict)
+                
+        return table_data, events
+            
+    def get_table_data(self, all_dfs: list):
+        player_dict = {}
+        team_dict = {}
+        events_dict = {}
+        for df in all_dfs:
+            # Checking to make sure it is leaderboard vs statistics
+            if len(df.columns) <= 6:
+                records = df.to_dict("records")
+                col_names = self.find_col_names(records)
+                table_data, events = self.find_table_data(col_names, records)
+                dict_name, table_name = self.find_table_name(records)
+                if dict_name == "Player":
+                    player_dict[table_name] = table_data
+                else:
+                    team_dict[table_name] = table_data
+                if events:
+                    events_dict = self.clean_events(events)
+                    
+        return player_dict, team_dict, events_dict
 
-    def clean_events(self, driver):
+    def clean_events(self, events):
         # TODO save events links and scrape that for winners
         events_dict = {}
-        row = None
-        try:
-            row = driver.find_element(By.XPATH, ".//td[contains(., 'Events') or contains(., 'Salary')]")
-        except:
-            pass
-        if not row:
-            return events_dict
+        seasonal_pattern = r"Seasonal Events:\s*(.*?)(?=Navigation|$)"
+        avg_salary_pattern = r"(Average\sSalary:\s[\d\$,.]+)"
+        min_salary_pattern = r"(Minimum\sSalary:\s[\d\$,.]+)"
+        seasonal_event = re.search(seasonal_pattern, events)
+        avg_salary = re.search(avg_salary_pattern, events)
+        min_salary = re.search(min_salary_pattern, events)
         
-        event_text = row.text.split("\n")
-        
-        for text in event_text:
-            text = text.split(": ")
-            title = text[0]
-            info = text[1].split(" | ")
-            if "Events" in title or "Salary" in title:
-                events_dict[title] = info
+        if seasonal_event:
+            events_label, events_list = seasonal_event.group().split(": ")
+            events_list = events_list.split(" | ")
+            events_dict[events_label] = events_list
+        if avg_salary:
+            avg_salary_label, avg_salary_amount = avg_salary.group().split(": $")
+            events_dict[avg_salary_label] = avg_salary_amount
+        if min_salary:
+            min_salary_label, min_salary_amount = min_salary.group().split(": $")
+            events_dict[min_salary_label] = min_salary_amount
         return events_dict
-        
-    # def get_event(self, driver):
-    #     search_results = driver.find_elements(By.CSS_SELECTOR, "table.boxed > tbody > tr")
-        
-    #     print(search_results)
-    
-    def log_data(self, links : list):
-        for link in links:
-            try:
-                driver.get(link)
-                sleep(2)
-            except Exception:
-                pass
-            year, league = self.get_year_league(driver)
+
+    def log_data(self, html_strings):
+        player_dict = {}
+        team_dict = {}
+        for html_string in html_strings:
+            year, league = self.get_year_league(html_string)
             if year and league:
-                player, team = self.get_data(driver)
-                self.player_stats[year][league] = player
-                self.team_stats[year][league] = team
-                if not self.events.get(year):
-                    events = self.clean_events(driver)
-                    self.events[year] = events
-            
+                print(f"Collecting data for {year} {league}")
+                all_dfs = self.find_tables(html_string)
+                player_dict, team_dict, events_dict = self.get_table_data(all_dfs)
+                if events_dict:
+                    events_dict["Year"] = year
+                    self.events[year] = events_dict
+
+                self.player_stats[year][league] = player_dict
+                self.team_stats[year][league] = team_dict
     
-    def convert_events_to_df(self, dictionary):
+    def convert_events_to_df(self, events_dict):
         # Events will have tables [Events, Salary]
         events_list = ["Special Events", "Salary"]
     
@@ -254,19 +269,20 @@ class Scraper():
         # Current list of tables for stats [Hitting Statistics, Pitching Statistics, Standings]
         for year, leagues in dictionary.items():
             for league, data in leagues.items():
+                # try:
                 for items in data.get("Hitting Statistics", []):
                     self.add_to_table(hit_table, items, year, league)
                 for items in data.get("Pitching Statistics", []):
                     self.add_to_table(pitch_table, items, year, league)
                 for items in data.get("Standings", []):
                     self.add_to_table(standing_table, items, year, league)
-                    
-        hit_stats = pd.DataFrame(hit_table)
-        pitch_stats = pd.DataFrame(pitch_table)
-        standing_stats = pd.DataFrame(standing_table)
+                # except Exception:
+                #     print(league)
+        hit_stats = pd.DataFrame(hit_table).dropna(axis=1, how='all')
+        pitch_stats = pd.DataFrame(pitch_table).dropna(axis=1, how='all')
+        standing_stats = pd.DataFrame(standing_table).dropna(axis=1, how='all')
 
         return hit_stats, pitch_stats, standing_stats
-        
 
     def add_to_table(self, table, items, year, league):
         if items:
@@ -274,6 +290,10 @@ class Scraper():
             stats["Year"] = year
             stats["League"] = league
             table.append(stats)
+            
+    # def clean_df(self, df):
+    #     cleaned_df = df.dropna(axis=1, how='all')
+    #     return cleaned_df
 
 if __name__ == "__main__":
     Scraper().scrape()
